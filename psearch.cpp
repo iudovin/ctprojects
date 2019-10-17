16f4190f586f88d102c51da978055c7477d4abb7
iudovin <36095978+iudovin@users.noreply.github.com>
2019-10-17 23:35:01 +0300

    Add files via upload

--- psearch.cpp
+++ psearch.cpp
@@ -10,18 +10,18 @@ const int BUF_SIZE = 4096;
 // THAT WORKS!!!
 void dfs(int * fd) {
     char buf2[BUF_SIZE] = {0};
-    fprintf(stderr, "Now I'm in %s\n", getcwd(buf2, sizeof(buf2)));
+    //fprintf(stderr, "Now I'm in %s\n", getcwd(buf2, sizeof(buf2)));
     DIR *dir = opendir(".");
     for (auto rd = readdir(dir); rd != NULL; rd = readdir(dir)) {
         if (rd->d_type == DT_DIR) {
             if (rd->d_name[0] == '.') continue;
-            fprintf(stderr, "I'm going to %s\n", rd->d_name);
+            //fprintf(stderr, "I'm going to %s\n", rd->d_name);
             chdir(rd->d_name);
             dfs(fd);
             chdir("..");
         }
         if (rd->d_type == DT_REG) {
-            fprintf(stderr, "%s ", rd->d_name);
+            //fprintf(stderr, "%s ", rd->d_name);
             FILE * f;
             f = fopen(rd->d_name, "r");
             char str[BUF_SIZE] = {0};
@@ -36,6 +36,11 @@ void dfs(int * fd) {
     }
 }
 
+int kmp(char * s, char * t) {  // s = string, t = text 
+	// returns number of entries of s in t
+	// > 0 if s is a substring of t
+	return 0;
+}
 
 int main() {
     close(0);
@@ -44,16 +49,18 @@ int main() {
     pipe(fd);
     pid_t pid = fork();
     if (pid == 0) {
+		close(fd[0]);
         dfs(fd);
     } else {
+		close(fd[1]);
         int rd;
         char buf[BUF_SIZE] = {0};
         while ((rd = read(fd[0], buf, sizeof(buf))) > 0) {
-            // KMP buf, t
-            fprintf(stderr, "< reading\n");
+            if (kmp(pattern, buf) > 0) {
+				fprintf(stderr, "Pattetn found!\n");
+			}
+            //fprintf(stderr, "< reading\n");
         }
-        fprintf(stderr, "pipe is read\n");
+        //fprintf(stderr, "pipe is read\n");
     }
-    //int fd[2] = {0};
-    //dfs(fd);
 }
