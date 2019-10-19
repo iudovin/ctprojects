38697b4564858f9dd68b18e74610ecbafc4d77bd
iudovin <36095978+iudovin@users.noreply.github.com>
2019-10-19 10:58:59 +0300

    Add files via upload

--- psearch.cpp
+++ psearch.cpp
@@ -4,24 +4,22 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <vector>
 
 const int BUF_SIZE = 4096;
 
 // THAT WORKS!!!
-void dfs(int * fd) {
+void dfs(int * fd, int * fdd) {
     char buf2[BUF_SIZE] = {0};
-    //fprintf(stderr, "Now I'm in %s\n", getcwd(buf2, sizeof(buf2)));
     DIR *dir = opendir(".");
     for (auto rd = readdir(dir); rd != NULL; rd = readdir(dir)) {
+        if (rd->d_name[0] == '.') continue;
         if (rd->d_type == DT_DIR) {
-            if (rd->d_name[0] == '.') continue;
-            //fprintf(stderr, "I'm going to %s\n", rd->d_name);
             chdir(rd->d_name);
-            dfs(fd);
+            dfs(fd, fdd);
             chdir("..");
         }
         if (rd->d_type == DT_REG) {
-            //fprintf(stderr, "%s ", rd->d_name);
             FILE * f;
             f = fopen(rd->d_name, "r");
             char str[BUF_SIZE] = {0};
@@ -31,36 +29,65 @@ void dfs(int * fd) {
                 str[i] = c;
                 i++;
             }
+            //strcat(str, "@");
+            //strcat(str, rd->d_name);
             write(fd[1], str, sizeof(str));
+            write(fdd[1], rd->d_name, sizeof(rd->d_name));
         }
     }
 }
 
-int kmp(char * s, char * t) {  // s = string, t = text 
+int kmp(char * s1, char * t) {  // s = string, t = text
 	// returns number of entries of s in t
 	// > 0 if s is a substring of t
-	return 0;
+    char s[BUF_SIZE] = {0};
+    strcpy(s, s1);
+    int m = strlen(s);
+    strcat(s, "@");
+    strcat(s, t);
+    int n = strlen(s);
+    int ans = 0;
+    std::vector<int> pi(n);
+    pi[0] = 0;
+    for (int i = 1; i < n; ++i) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+        if (pi[i] == m) {
+            ans++;
+        }
+    }
+    return ans;
 }
 
 int main() {
+    char pattern[BUF_SIZE] = {0};
+    scanf("%s", pattern);
     close(0);
     close(1);
     int fd[2];
+    int fdd[2];
     pipe(fd);
+    pipe(fdd);
     pid_t pid = fork();
     if (pid == 0) {
 		close(fd[0]);
-        dfs(fd);
+        close(fdd[0]);
+        dfs(fd, fdd);
     } else {
 		close(fd[1]);
-        int rd;
+        close(fdd[1]);
+        int rd, rdd;
         char buf[BUF_SIZE] = {0};
-        while ((rd = read(fd[0], buf, sizeof(buf))) > 0) {
+        char name[BUF_SIZE] = {0};
+        while ((rd = read(fd[0], buf, sizeof(buf))) > 0 && (rdd = read(fdd[0], name, sizeof(name))) > 0) {
             if (kmp(pattern, buf) > 0) {
-				fprintf(stderr, "Pattetn found!\n");
+				fprintf(stderr, "Pattetn found in %s\n", name);
 			}
-            //fprintf(stderr, "< reading\n");
+            fprintf(stderr, "reading:  %s\n", name);
         }
-        //fprintf(stderr, "pipe is read\n");
+        fprintf(stderr, "pipe is read\n");
     }
 }
